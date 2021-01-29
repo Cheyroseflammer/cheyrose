e0e0003a57c440e28362eda2cb3e50dae55e040c
Chey Rose <cheyroseflammer@gmail.com>
2021-01-29 08:02:04 -0800

    additional

--- script.js
+++ script.js
@@ -6,9 +6,32 @@ $(document).ready(function () {
       $(".navbar").removeClass("sticky");
     }
   });
-  // toggle
+  // TOGGLE MENU / NAVBAR SCRIPT //
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
+  });
+  // OWL CAROUSEL SCRIPT //
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    dots: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
